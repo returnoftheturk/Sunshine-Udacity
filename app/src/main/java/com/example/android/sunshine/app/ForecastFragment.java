7e5380e59c0f3329c63e88b4e2e8c10f03494974
Akgul <akgulahmet@outlook.com>
2016-07-30 00:12:57 -0400

    Add intent to open detail activity page upon clicking on any item on the list view.

--- app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
+++ app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
@@ -1,5 +1,6 @@
 package com.example.android.sunshine.app;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -15,7 +16,6 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -108,10 +108,14 @@ public class ForecastFragment extends Fragment {
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Toast.makeText(getActivity(), myForecastAdapter.getItem(i), Toast.LENGTH_SHORT).show();
+                //Toast.makeText(getActivity(), myForecastAdapter.getItem(i), Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(getActivity(), DetailActivity.class)
+                    .putExtra(Intent.EXTRA_TEXT, myForecastAdapter.getItem(i));
+                startActivity(intent);
             } 
         }
         );
+
 
 
         return rootView;
