65abfde1685847eed32557e97d75682f98f88709
Akgul <akgulahmet@outlook.com>
2016-08-02 12:46:26 -0400

    Add menu item to open location on a map using implicit intent.

--- app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -1,16 +1,18 @@
 package com.example.android.sunshine.app;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-
-
 public class MainActivity extends ActionBarActivity {
-
+    private final String LOG_TAG = MainActivity.class.getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,8 +43,30 @@ public class MainActivity extends ActionBarActivity {
             startActivity(new Intent(this, SettingsActivity.class));
             return true;
         }
+        if (id == R.id.action_map){
+            openLocationOnMap();
+            return true;
+        }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void openLocationOnMap(){
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        String units = sharedPreferences.getString(getString(R.string.pref_location_key),
+                                                getString(R.string.pref_location_default));
+        Uri uri = Uri.parse("geo:0,0?")
+                .buildUpon().appendQueryParameter("q", units).build();
+        intent.setData(uri);
+        Log.v("", "String units: " + units);
+        Log.v("", "URI uri: " + uri);
+
+        if (intent.resolveActivity(getPackageManager())!=null){
+            startActivity(intent);
+        }
+        else Log.e(LOG_TAG, "Couldn't open location: " + units + "no map application installed");
+
     }
 
 
