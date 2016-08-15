c1f5921d00e537f9a82422fe129881a10303a92f
Ahmet Akgul <akgulahmet@outlook.com>
2016-08-14 21:57:56 -0400

    Implement addLocation by looking at udacity code unfortunately

--- app/src/main/java/com/example/android/sunshine/app/FetchWeatherTask.java
+++ app/src/main/java/com/example/android/sunshine/app/FetchWeatherTask.java
@@ -15,9 +15,13 @@
  */
 package com.example.android.sunshine.app;
 
+import android.content.ContentResolver;
+import android.content.ContentUris;
 import android.content.ContentValues;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.preference.PreferenceManager;
@@ -25,7 +29,9 @@ import android.text.format.Time;
 import android.util.Log;
 import android.widget.ArrayAdapter;
 
+import com.example.android.sunshine.app.data.WeatherContract;
 import com.example.android.sunshine.app.data.WeatherContract.WeatherEntry;
+import com.example.android.sunshine.app.data.WeatherDbHelper;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -106,10 +112,38 @@ public class FetchWeatherTask extends AsyncTask<String, Void, String[]> {
      * @return the row ID of the added location.
      */
     long addLocation(String locationSetting, String cityName, double lat, double lon) {
-        // Students: First, check if the location with this city name exists in the db
-        // If it exists, return the current ID
-        // Otherwise, insert it using the content resolver and the base URI
-        return -1;
+        long id;
+
+        Cursor cur = mContext.getContentResolver()
+                .query(
+                        WeatherContract.LocationEntry.CONTENT_URI,
+                        new String[]{WeatherContract.LocationEntry._ID},
+                        WeatherContract.LocationEntry.COLUMN_LOCATION_SETTING+"=?",
+                        new String[]{locationSetting},
+                        null
+                        );
+
+        if (cur.moveToFirst()){
+            int locationIdIndex = cur.getColumnIndex(WeatherContract.LocationEntry._ID);
+            id = cur.getLong(locationIdIndex);
+        }
+        else{
+            ContentValues values = new ContentValues();
+            values.put(WeatherContract.LocationEntry.COLUMN_LOCATION_SETTING, locationSetting);
+            values.put(WeatherContract.LocationEntry.COLUMN_CITY_NAME, cityName);
+            values.put(WeatherContract.LocationEntry.COLUMN_COORD_LAT, lat);
+            values.put(WeatherContract.LocationEntry.COLUMN_COORD_LONG, lon);
+//            mContext.getContentResolver().insert(WeatherContract.LocationEntry.CONTENT_URI, values);
+//            mContext.getContentResolver().update(WeatherContract.LocationEntry.CONTENT_URI, values, null, null);
+            Uri insertedUri = mContext.getContentResolver().insert(WeatherContract.LocationEntry.CONTENT_URI, values);
+
+            id = ContentUris.parseId(insertedUri);
+
+        }
+        cur.close();
+        return id;
+
+
     }
 
     /*
