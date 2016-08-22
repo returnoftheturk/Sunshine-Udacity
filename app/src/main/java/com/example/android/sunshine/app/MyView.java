c29641f47afc4ddb7da2ea0321d08f06d9084b55
Ahmet Akgul <akgulahmet@outlook.com>
2016-08-22 16:37:31 -0400

    Add talkback feature to speak outloud the direction when compass is clicked upon.

--- app/src/main/java/com/example/android/sunshine/app/MyView.java
+++ app/src/main/java/com/example/android/sunshine/app/MyView.java
@@ -9,6 +9,8 @@ import android.graphics.Path;
 import android.graphics.RectF;
 import android.util.AttributeSet;
 import android.view.View;
+import android.view.accessibility.AccessibilityEvent;
+import android.view.accessibility.AccessibilityManager;
 
 /**
  * Created by Owner on 8/21/2016.
@@ -29,6 +31,7 @@ public class MyView extends View {
         } finally {
             a.recycle();
         }
+
         init();
     }
 
@@ -40,13 +43,52 @@ public class MyView extends View {
         } finally {
             a.recycle();
         }
+
         init();
     }
 
     public void setDirection(float degrees){
         mDirection = degrees;
+
+        AccessibilityManager accessibilityManager = (AccessibilityManager) getContext().getSystemService(Context.ACCESSIBILITY_SERVICE);
+        if (accessibilityManager.isEnabled()){
+
+            sendAccessibilityEvent(AccessibilityEvent.TYPE_VIEW_FOCUSED);
+        }
+
         invalidate();
         requestLayout();
+    }
+
+    @Override
+    public boolean dispatchPopulateAccessibilityEvent(AccessibilityEvent event) {
+//        return super.dispatchPopulateAccessibilityEvent(event);
+        event.getText().add(getDirectionDesc(mDirection));
+        return true;
+    }
+
+    private String getDirectionDesc(float degrees){
+        String windDirectionDesc = "Unknown";
+        if (degrees >= 337.5 || degrees < 22.5) {
+            windDirectionDesc = "North";
+        } else if (degrees >= 22.5 && degrees < 67.5) {
+            windDirectionDesc = "North East";
+        } else if (degrees >= 67.5 && degrees < 112.5) {
+            windDirectionDesc = "East";
+        } else if (degrees >= 112.5 && degrees < 157.5) {
+            windDirectionDesc = "South East";
+        } else if (degrees >= 157.5 && degrees < 202.5) {
+            windDirectionDesc = "South";
+        } else if (degrees >= 202.5 && degrees < 247.5) {
+            windDirectionDesc = "South West";
+        } else if (degrees >= 247.5 && degrees < 292.5) {
+            windDirectionDesc = "West";
+        } else if (degrees >= 292.5 && degrees < 337.5) {
+            windDirectionDesc = "North West";
+        }
+
+        return windDirectionDesc;
+
     }
 
     @Override
@@ -81,6 +123,7 @@ public class MyView extends View {
     Paint myArrowPaint;
     RectF myRectF;
     float mDirection;
+    String directionDesc;
 
     public void init(){
         myPaint = new Paint();
